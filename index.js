c96b5cbf22bf0bc01b7fc7da31c22343cbcc9320
liuwenbing <572906809@qq.com>
2021-02-20 19:54:35 +0800

    feat: 第一次提交

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 
-console.log('my onetothere!')
+const program = require('commander');
+const helpOptions = require('./lib/core/help')
+
+program.version(require('./package.json').version);
+
+// 给 -h 添加其他选项
+helpOptions();
+
+// 创建命令
+
+console.log('my onetothere!');
+
+// 解析终端指令
+program.parse(process.argv);
 
